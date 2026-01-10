30fc1b689b464a27afdf61cf67303bed3464fcb9
twuijri <twuijri@MacBook-Pro-twuijri.local>
2026-01-10 20:49:06 +0300

    Fix Turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  webpack: (config, { dev, isServer }) => {
-    return config;
-  },
 };
 
 export default nextConfig;
